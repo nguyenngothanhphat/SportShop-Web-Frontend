8cce1742f97d1b67d4bbc999c901634a98bdad8e
nguyenngothanhphat <thanhphat19@gmail.com>
2021-01-26 10:46:13 +0700

    fix: 🐛 remove admin folder

--- src/Routes.js
+++ src/Routes.js
@@ -5,7 +5,6 @@ import Register from "./components/auth/register";
 import home from './components/home/index';
 import Contact from './components/contact/index';
 import SingleProduct from './components/singleproduct/index'
-import UserDashboard from './admin/components/dashboard/userDashboard'
 import Cart from './components/cart/index';
 
 const Routes = () => {
@@ -19,12 +18,7 @@ const Routes = () => {
                         <Route path="/register" exact component={Register} />
                         <Route path="/contact" exact component={Contact} />
                         <Route path="/detail" exact component={SingleProduct} />
-                        <Route path="/cart" exact component={Cart} />
-                    </Switch>
-                </BrowserRouter>
-                <BrowserRouter>
-                    <Switch>
-                        <Route path="/admin" exact component={UserDashboard} />
+                        <Route path="/carts" exact component={Cart} />
                     </Switch>
                 </BrowserRouter>
             </div>
