d669bc87dbc6d2f5109fe2408dddc81d8cccd3fd
Thanh Phat <thanhphat19@gmail.com>
2021-03-16 13:03:44 +0700

    feat: 🎸 set filters with category & radiobox for price range

--- src/components/core/checkBox/checkBox.js
+++ src/components/core/checkBox/checkBox.js
@@ -1,32 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const CheckBox = ({ categories }) => {
-    console.log("🚀 ~ file: checkBox.js ~ line 4 ~ CheckBox ~ categories", categories)
-    const [checked, setChecked] = useState([]);
+const CheckBox = ({ categories, handleFilters }) => {
+  console.log(
+    "🚀 ~ file: checkBox.js ~ line 4 ~ CheckBox ~ categories",
+    categories
+  );
+  const [checked, setChecked] = useState([]);
 
-    const handleToggle = c => () => {
-        const currentCategoryId = checked.indexOf(c)
-        const newCheckedCategoryId = [...checked]
+  const handleToggle = (c) => () => {
+    const currentCategoryId = checked.indexOf(c);
+    const newCheckedCategoryId = [...checked];
 
-        if (currentCategoryId === -1) {
-            newCheckedCategoryId.push(c);
-        } else {
-            newCheckedCategoryId.splice(currentCategoryId, 1)
-        }
-
-        console.log("🚀 ~ file: checkBox.js ~ line 9 ~ CheckBox ~ newCheckedCategoryId", newCheckedCategoryId)
-        setChecked(newCheckedCategoryId)
+    if (currentCategoryId === -1) {
+      newCheckedCategoryId.push(c);
+    } else {
+      newCheckedCategoryId.splice(currentCategoryId, 1);
     }
-    return (
-        <div>
-            {categories.data && categories.data.map((c, i) => (
-                <li key={i} className="list-unstyled">
-                    <input onChange={handleToggle(c._id)} value={checked.indexOf(c._id === -1)} type="checkbox" className="form-check-input" />
-                    <label className="form-check-label">{c.categoryName}</label>
-                </li>
-            ))}
-        </div>
-    )
-}
+
+    console.log(
+      "🚀 ~ file: checkBox.js ~ line 9 ~ CheckBox ~ newCheckedCategoryId",
+      newCheckedCategoryId
+    );
+    setChecked(newCheckedCategoryId);
+    handleFilters(newCheckedCategoryId);
+  };
+  return (
+    <div>
+      {categories.data &&
+        categories.data.map((c, i) => (
+          <li key={i} className="list-unstyled">
+            <input
+              onChange={handleToggle(c._id)}
+              value={checked.indexOf(c._id === -1)}
+              type="checkbox"
+              className="form-check-input"
+            />
+            <label className="form-check-label">{c.categoryName}</label>
+          </li>
+        ))}
+    </div>
+  );
+};
 
 export default CheckBox;
--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -1,48 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import Card from '../core/Card/card';
-import CheckBox from '../core/checkBox/checkBox';
+import React, { useState, useEffect } from "react";
+import Card from "../core/Card/card";
+import CheckBox from "../core/checkBox/checkBox";
+import RadioBox from '../core/radioBox/radioBox';
+import {prices} from '../core/fixedPrices/fixedPrices'
 
-import { getCategories } from '../../util/api/category-apis';
+import { getCategories } from "../../util/api/category-apis";
 
-import Header from '../header/index'
+import Header from "../header/index";
 
 const Filter = () => {
-    const [categories, setCategories] = useState([]);
-    const [error, setError] = useState(false);
+  const [myFilters, setMyFilters] = useState({
+    filters: { category: [], price: [] },
+  });
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(false);
 
-    const init = () => {
-        getCategories().then(data => {
-            console.log("🚀 ~ file: filter.js ~ line 15 ~ getCategories ~ data", data)
-            if (data.error) {
-                setError(data.error)
-            } else {
-                setCategories(data)
-            }
-        })
-    }
+  const init = () => {
+    getCategories().then((data) => {
+      console.log(
+        "🚀 ~ file: filter.js ~ line 15 ~ getCategories ~ data",
+        data
+      );
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setCategories(data);
+      }
+    });
+  };
 
-    useEffect(() => {
-        init();
-    }, []);
-    return (
-        <div>
-            <Header />
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-4">
-                        <h4>Filter by Categories</h4>
-                        {/* {categories ? console.log(categories) : null} */}
-                        <ul>
-                            <CheckBox categories={categories} />
-                        </ul>
-                    </div>
-                    <div className="col-8">
-                        right
-                    </div>
-                </div>
-            </div>
+  useEffect(() => {
+    init();
+  }, []);
+
+  const handleFilters = (filters, filterBy) => {
+    // console.log("SHOP: ", filters, filterBy);
+    const newFilters = { ...myFilters };
+    newFilters.filters[filterBy] = filters;
+    setMyFilters(newFilters);
+   };
+
+  return (
+    <div>
+      <Header />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-4">
+            <h4>Filter by Categories</h4>
+            {/* {categories ? console.log(categories) : null} */}
+            <ul>
+              <CheckBox
+                categories={categories}
+                handleFilters={(filters) => handleFilters(filters, "category")}
+              />
+            </ul>
+
+            <h4>Filter by Price Range</h4>
+            {/* {categories ? console.log(categories) : null} */}
+            <ul>
+              <RadioBox
+                prices={prices}
+                handleFilters={(filters) => handleFilters(filters, "price")}
+              />
+            </ul>
+          </div>
+          <div className="col-8">
+              {JSON.stringify(myFilters)}
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Filter;
