be369f61e7fc48d3594b27ba9043cd74c675c437
Thành Phát <thanhphat19@gmail.com>
2021-03-18 21:51:33 +0700

    feat: 🎸 user logout & login page

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useDispatch } from 'react-redux';
 
 import Header from './components/nav/header';
 import Home from './components/home/home';
@@ -10,7 +11,27 @@ import Register from "./components/auth/register";
 import ResgisterComplete from './components/auth/registerComplete'
 import './styles.css'
 
+import { auth } from './config/firebase';
+
 const Routes = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const unsubcribe = auth.onAuthStateChanged(async (user) => {
+            if (user) {
+                const idTokenResult = await user.getIdTokenResult();
+                console.log("🚀 ~ file: Routes.js ~ line 22 ~ unsubcribe ~ user", user);
+                dispatch({
+                    type: "LOGGED_IN_USER",
+                    payload: {
+                        email: user.email,
+                        token: idTokenResult.token,
+                    }
+                })
+            }
+        })
+        return () => unsubcribe()
+    }, [])
     return (
         <>
             <Header />
