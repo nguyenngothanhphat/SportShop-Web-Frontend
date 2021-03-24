e1565b0b417743fb2e4237eb7bc03894eb8a06e6
Thành Phát <thanhphat19@gmail.com>
2021-03-25 00:32:38 +0700

    feat: 🎸 Filter By Star Rating and By Sub Categories

--- src/components/card/cardProduct.js
+++ src/components/card/cardProduct.js
@@ -10,7 +10,7 @@ const { Meta } = Card;
 
 const CardProduct = ({ product }) => {
   // destructure
-  const { images, title, description, slug } = product;
+  const { images, title, description, slug, price } = product;
   return (
     <>
       <Card
@@ -37,7 +37,7 @@ const CardProduct = ({ product }) => {
           <div><StarRating starDimension="20px" starSpacing="2px" editing={false} />{" "} (0)</div>
         )}
         <Meta
-          title={title}
+          title={`${title} - $${price}`}
           description={`${description && description.substring(0, 40)}...`}
         />
       </Card>
--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -3,10 +3,13 @@ import {
   getProductsByCount,
   fetchProductsByFilter,
 } from "../../util/api/product-apis";
+import { getCategories } from '../../util/api/category-apis'
+import { getSubs } from '../../util/api/sub-apis'
 import { useSelector, useDispatch } from "react-redux";
 import CardProduct from "../card/cardProduct";
-import { Menu, Slider } from "antd";
-import { DollarOutlined } from "@ant-design/icons";
+import { Menu, Slider, Checkbox } from "antd";
+import { DollarOutlined, DownSquareOutlined, StarOutlined } from "@ant-design/icons";
+import Star from '../star/star';
 
 import Header from "../nav/header";
 
@@ -14,8 +17,16 @@ const { SubMenu, ItemGroup } = Menu;
 
 const Filter = () => {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [categoryIds, setCategoryIds] = useState([]);
+  const [star, setStar] = useState("");
   const [loading, setLoading] = useState(false);
   const [price, setPrice] = useState([0, 0]);
+  const [ok, setOk] = useState(false);
+  const [subs, setSubs] = useState([]);
+  const [sub, setSub] = useState("");
+
+  let dispatch = useDispatch();
 
   let { search } = useSelector((state) => ({ ...state }));
 
@@ -23,6 +34,9 @@ const Filter = () => {
 
   useEffect(() => {
     loadAllProducts();
+    loadCategories();
+    // fetch subcategories
+    getSubs().then((res) => setSubs(res.data));
   }, []);
 
   /* Load products by default on page load */
@@ -32,6 +46,10 @@ const Filter = () => {
       setLoading(false);
     });
   };
+
+  const loadCategories = () => {
+    getCategories().then(res => setCategories(res.data))
+  }
 
   /* Load products on user search input */
   useEffect(() => {
@@ -47,15 +65,128 @@ const Filter = () => {
     });
   };
 
+  /* Load products based on price range */
+  useEffect(() => {
+    console.log("ok to request products");
+    fetchProducts({ price });
+  }, [ok]);
+
+  const handleSlider = (value) => {
+    dispatch({
+      type: "SEARCH_QUERY",
+      payload: { text: "" },
+    });
+    // reset
+    setCategoryIds([]);
+    setPrice(value);
+    setStar("");
+    setSub("");
+    setTimeout(() => {
+      setOk(!ok);
+    }, 300);
+  };
+
+  // show categories in a list of checkbox
+  const showCategories = () =>
+    categories.map((c) => (
+      <div key={c._id}>
+        <Checkbox
+          onChange={handleCheck}
+          className="pb-2 pl-4 pr-4"
+          value={c._id}
+          name="category"
+          checked={categoryIds.includes(c._id)}
+        >
+          {c.name}
+        </Checkbox>
+        <br />
+      </div>
+    ));
+
+  // handle check for categories
+  const handleCheck = (e) => {
+    dispatch({
+      type: "SEARCH_QUERY",
+      payload: { text: "" },
+    });
+    setPrice([0, 0]);
+    // console.log(e.target.value);
+    let inTheState = [...categoryIds];
+    let justChecked = e.target.value;
+    let foundInTheState = inTheState.indexOf(justChecked); // index or -1
+
+    // indexOf method ?? if not found returns -1 else return index [1,2,3,4,5]
+    if (foundInTheState === -1) {
+      inTheState.push(justChecked);
+    } else {
+      // if found pull out one item from index
+      inTheState.splice(foundInTheState, 1);
+    }
+
+    setCategoryIds(inTheState);
+    // console.log(inTheState);
+    fetchProducts({ category: inTheState });
+  };
+
+  const handleStarClick = (num) => {
+    // console.log(num);
+    dispatch({
+      type: "SEARCH_QUERY",
+      payload: { text: "" },
+    });
+    setPrice([0, 0]);
+    setCategoryIds([]);
+    setStar(num);
+    setSub("");
+    fetchProducts({ stars: num });
+  };
+
+  const showStars = () => (
+    <div className="pr-4 pl-4 pb-2">
+      <Star starClick={handleStarClick} numberOfStars={5} />
+      <Star starClick={handleStarClick} numberOfStars={4} />
+      <Star starClick={handleStarClick} numberOfStars={3} />
+      <Star starClick={handleStarClick} numberOfStars={2} />
+      <Star starClick={handleStarClick} numberOfStars={1} />
+    </div>
+  );
+
+  // 6. show products by sub category
+  const showSubs = () =>
+    subs.map((s) => (
+      <div
+        key={s._id}
+        onClick={() => handleSub(s)}
+        className="p-1 m-1 badge badge-secondary"
+        style={{ cursor: "pointer" }}
+      >
+        {s.name}
+      </div>
+    ));
+
+  const handleSub = (sub) => {
+    // console.log("SUB", sub);
+    setSub(sub);
+    dispatch({
+      type: "SEARCH_QUERY",
+      payload: { text: "" },
+    });
+    setPrice([0, 0]);
+    setCategoryIds([]);
+    setStar("");
+    fetchProducts({ sub });
+  };
+
   return (
     <>
       <Header />
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-3">
+          <div className="col-md- pt-2">
             <h4>Search/Filter</h4>
             <hr />
-            <Menu defaultOpenKeys={["1", "2"]} mode="inline">
+            <Menu defaultOpenKeys={["1", "2", "3", "4"]} mode="inline">
+              {/* Price range */}
               <SubMenu
                 key="1"
                 title={
@@ -70,9 +201,47 @@ const Filter = () => {
                     tipFormatter={(v) => `$${v}`}
                     range
                     value={price}
-                    onChange={(value) => setPrice(value)}
+                    onChange={handleSlider}
                     max="4990"
                   />
+                </div>
+              </SubMenu>
+
+              {/* Filter categories */}
+              <SubMenu
+                key="2"
+                title={
+                  <span className="h6">
+                    <DownSquareOutlined /> Categories
+                  </span>
+                }
+              >
+                <div style={{ maringTop: "-10px" }}>{showCategories()}</div>
+              </SubMenu>
+
+              {/* stars */}
+              <SubMenu
+                key="3"
+                title={
+                  <span className="h6">
+                    <StarOutlined /> Rating
+                </span>
+                }
+              >
+                <div style={{ maringTop: "-10px" }}>{showStars()}</div>
+              </SubMenu>
+
+              {/* sub category */}
+              <SubMenu
+                key="4"
+                title={
+                  <span className="h6">
+                    <DownSquareOutlined /> Sub Categories
+                </span>
+                }
+              >
+                <div style={{ maringTop: "-10px" }} className="pl-4 pr-4">
+                  {showSubs()}
                 </div>
               </SubMenu>
             </Menu>
