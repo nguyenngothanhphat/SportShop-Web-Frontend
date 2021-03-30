90f88467c5bb48662ab8ce7e6129f6f95a5b4688
Thanh Phat <thanhphat19@gmail.com>
2021-03-30 10:21:43 +0700

    style: 💄 re-style user interface

--- src/components/nav/header.js
+++ src/components/nav/header.js
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import { Anchor, Drawer, Button, Badge } from "antd";
 import firebase from "firebase";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 
-import Search from '../search/search';
+import Search from "../search/search";
 
-const { Link } = Anchor;
+// const { Link } = Anchor;
 
 const Header = () => {
   const [visible, setVisible] = useState(false);
@@ -91,60 +91,89 @@ const Header = () => {
     //   </span>
     // </Menu>
 
-    <div className="container-fluid">
-      <div className="header">
-        <div className="logo">
-          <i className="fas fa-bolt"></i>
-          <a href="/">Thanh Phat</a>
-        </div>
-        <div className="mobileHidden">
-          <Anchor targetOffset="65">
-            <Link href="/" title="Home" />
-            <Link href="/filter/product" title="Promotions" />
-            <Link href="/filter/product" title="Product" />
-            <Link href="/filter/product" title="Accessories" />
-            <Link href="#" title="Blog" />
-            <Link href="#" title="Contact us" />
-            <Badge count={cart.length} offset={[9, 0]} style={{ right: "9px" }}>
-              <Link href="/cart" title="Cart" />
-            </Badge>
-            {!user && (
-              <Link href="/login" title="Login" />
-            )}
+    // <div className="container-fluid">
+    //   <div className="header">
+    //     <div className="logo">
+    //       <i className="fas fa-bolt"></i>
+    //       <a href="/">Thanh Phat</a>
+    //     </div>
+    //     <div className="mobileHidden">
+    //       <Anchor targetOffset="65">
+    //         <Link href="/" title="Home" />
+    //         <Link href="/filter/product" title="Promotions" />
+    //         <Link href="/filter/product" title="Product" />
+    //         <Link href="/filter/product" title="Accessories" />
+    //         <Link href="#" title="Blog" />
+    //         <Link href="#" title="Contact us" />
+    //         <Badge count={cart.length} offset={[9, 0]} style={{ right: "9px" }}>
+    //           <Link href="/cart" title="Cart" />
+    //         </Badge>
+    //         {!user && <Link href="/login" title="Login" />}
 
-            {!user && (
-              <Link href="/register" title="Register" />
-            )}
+    //         {!user && <Link href="/register" title="Register" />}
 
-            {user && (
-              <Link title={user.email && user.email.split("@")[0]} />
-            )}
+    //         {user && <Link title={user.email && user.email.split("@")[0]} />}
 
-          </Anchor>
-        </div>
-        <div className="mobileVisible">
-          <Button type="primary" onClick={showDrawer}>
-            <i className="fas fa-bars"></i>
-          </Button>
-          <Drawer
-            placement="right"
-            closable={false}
-            onClose={onClose}
-            visible={visible}
-          >
-            <Anchor targetOffset="65">
-              <Link href="#hero" title="Home" />
-              <Link href="#about" title="About" />
-              <Link href="#feature" title="Features" />
-              <Link href="#works" title="How it works" />
-              <Link href="#faq" title="FAQ" />
-              <Link href="#pricing" title="Pricing" />
-              <Link href="#contact" title="Contact" />
-            </Anchor>
-          </Drawer>
+    //       </Anchor>
+    //     </div>
+    //     <div className="mobileVisible">
+    //       <Button type="primary" onClick={showDrawer}>
+    //         <i className="fas fa-bars"></i>
+    //       </Button>
+    //       <Drawer
+    //         placement="right"
+    //         closable={false}
+    //         onClose={onClose}
+    //         visible={visible}
+    //       >
+    //         <Anchor targetOffset="65">
+    //           <Link href="#hero" title="Home" />
+    //           <Link href="#about" title="About" />
+    //           <Link href="#feature" title="Features" />
+    //           <Link href="#works" title="How it works" />
+    //           <Link href="#faq" title="FAQ" />
+    //           <Link href="#pricing" title="Pricing" />
+    //           <Link href="#contact" title="Contact" />
+    //         </Anchor>
+    //       </Drawer>
+    //     </div>
+    //   </div>
+    // </div>
+    <header>
+      <div id="top-header">
+        <div className="container">
+          <ul className="header-links pull-left">
+            <li>
+              <Link>
+                <i className="fa fa-phone"></i> +021-95-51-84
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <i className="fa fa-envelope-o"></i> thanhphat19@gmail.com
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <i className="fa fa-map-marker"></i> Pham Phu Thu
+              </Link>
+            </li>
+          </ul>
+          <ul className="header-links pull-right">
+            <li>
+              <Link>
+                <i className="fa fa-dollar"></i> USD
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <i className="fa fa-user-o"></i> My Account
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
-    </div>
+    </header>
   );
 };
 
